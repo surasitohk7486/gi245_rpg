d7224d762a8345569165282fa19d53eab63746b8
Nangil <surasit.dang@bumail.net>
2025-03-14 20:15:26 +0700

    Week6 14.19

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,14 +1,56 @@
+using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     [SerializeField] private RectTransform selectionBox;
     public RectTransform SelectionBox { get { return selectionBox; } }
 
+    [SerializeField]
+    private Toggle togglePauseUnpause;
+
     public static UIManager instance;
 
     private void Awake()
     {
         instance = this;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+            togglePauseUnpause.isOn = !togglePauseUnpause.isOn;
+    }
+
+    public void ToggleAI(bool isOn)
+    {
+        foreach (Characters member in PartyManager.instance.Members)
+        {
+            AttackAI ai = member.gameObject.GetComponent<AttackAI>();
+
+            if (ai != null)
+            {
+                ai.enabled = isOn;
+            }
+        }
+    }
+
+    public void SelectAll()
+    {
+        foreach (Characters member in PartyManager.instance.Members)
+        {
+            if (member.CurHP > 0)
+            {
+                member.ToggleRingSelection(true);
+                PartyManager.instance.SelectChars.Add(member);
+
+            }
+        }
+    }
+
+    public void PauseUnpause(bool isOn)
+    {
+        Time.timeScale = isOn ? 0 : 1;
     }
 }
